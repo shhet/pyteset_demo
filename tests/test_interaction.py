68d2a6b343dac6dee0ff1ac1463f1976ee717834
shhet <agwndm2401@gmail.com>
2023-04-08 09:12:24 +0900

    namespace

--- tests/test_interaction.py
+++ tests/test_interaction.py
@@ -8,8 +8,19 @@ def test_main(mocker):
     # https://gist.github.com/aodag/08d63828bb582fdb7599014d637cb863
     mocker.patch('interaction.A.func_a')
     mocker.patch('interaction.B.func_b', return_value=11)
-# mocker.patch('module_a.A.func_a')
+    # mocker.patch('module_a.A.func_a')
     # mocker.patch('module_b.B.func_b', return_value=1)
     
     res = Main().main()
     assert res == 100
+
+def test_main2(mocker):
+    # テスト対象のモジュールから見たときの名前空間を記載する
+    # https://gist.github.com/aodag/08d63828bb582fdb7599014d637cb863
+    # mocker.patch('interaction.A.func_a')
+    # mocker.patch('interaction.B.func_b', return_value=11)
+    mocker.patch('module_a.A.func_a')
+    mocker.patch('module_b.B.func_b', return_value=-1)
+    
+    res = Main().main()
+    assert res == -100
